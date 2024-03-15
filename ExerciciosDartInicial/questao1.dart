ed12ee13cb95e839eebceb3023e781c01213607b
Nataniel de Aguiar da Silva <nataniel.natanaguiar@gmail.com>
2024-03-15 13:20:22 -0300

    Enviado exercícios

---
+++ ExerciciosDartInicial/questao1.dart
@@ -0,0 +1,9 @@
+/*
+1. Declaração e Utilização de Variáveis
+a) Declare uma variável chamada nome e atribua a ela o valor "John Doe".
+b) Em seguida, imprima a mensagem "Bem-vindo, [nome]!" onde [nome] é o valor da variável nome.
+*/
+main() {
+  String nome = "Jonh Doe";
+  print("Seu nome é ${nome}");
+}
